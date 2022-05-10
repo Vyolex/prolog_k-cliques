4be0526347071e1a242bab3cc96280c595221f75
lderoeck <32436336+lderoeck@users.noreply.github.com>
2022-05-10 22:44:44 +0200

    Documentation

--- README.md
+++ README.md
@@ -1,21 +1,39 @@
 # k-cliques problem in Prolog
-Programming paradigms prolog assignment
+Programming paradigms Prolog assignment
 
-The purpose of this program is to solve the k-cliques problem for an adjacency matrix.
+The purpose of this program is to solve the K-cliques problem in a undirected graph. The graph gets input as an adjacency matrix, because it is undirected, the matrix will be symmetrical. 
 
+## Usage
+The graph is defined and hardcoded in [main.pl](./src/main.pl). To change which graph is being tested, modify the hardcoded graph.
 
-
-## Find k cliques in graph
-To find if there is a k-clique in a graph, we generate each possible unique permutation of nodes of a given length and check if there is one where all the nodes connect to eachother.
+### K-cliques
+Deterimines if there is a K-clique in the graph. 
 
 ```
     findKClique(k).
 ```
 
-## Find largest clique in a graph
-Iteratively run increasing k-clique tests to find the largest possible.
+#### Method
+To find if there is a K-clique in a graph, we generate each possible permutation of nodes of a given length (K) and check if there is one where all the nodes connect to eachother. If no such permutation exists, then there is no K-clique present in the graph.
+
+##### Optimisations
+- K values higher than node amount fail immediately.
+- Unique permutations: no repeating numbers used in permutation generation.
+
+
+### Max clique
+Finds the maximum size clique present in the graph. 
 
 ```
     dumbFindLargest(0,X).
 ```
+The first argument in the rule is the starting value. 
+
+#### Method
+To find the maximum clique size, we simply iteratively compute increasing K-clique computations, when it fails, we know the maximum clique is one below the current K value. This is because no K clique can exist without a K-1 clique existing, which means we do not need to search further to find bigger cliques after the first failure.
+
+
+
+## Requirements
+This program has been written for the [SWI-Prolog](https://www.swi-prolog.org/) interpreter and uses some of it's build in predicates. It has not been verified to work with other Prolog interpreters.
 
--- src/main.pl
+++ src/main.pl
@@ -2,6 +2,7 @@ use_module(library(lists)).
 
 /*
     Adjacency matrix graph
+    Edit matrix to compute for different graphs.
 */
 mygraph(
     [[0,1,1,1,1,0,0],
@@ -10,19 +11,24 @@ mygraph(
     [1,1,1,0,0,1,0],
     [1,0,0,0,0,1,0],
     [0,0,1,1,1,0,0],
-    [0,1,0,0,0,0,0]]).
+    [0,1,0,0,0,0,0]]
+    ).
 
 /*
     Rule that finds k-clique in graph
 */
 findKClique(K) :- 
-    mygraph(L),
-    listLength(L, N),
-    Ns is N-1,
-    numlist(0, Ns, Xs),
-    uniquePermutation(K, Xs, Ls),
-    % print(Ls),
-    allConnected(Ls).
+    mygraph(L),                         % Load graph in L
+    listLength(L, N),                   % Compute length of graph (#nodes) and store in N
+    (
+        K > N -> false                  % Check if K is larger than N (#nodes), if so return false (no cliques can exist with more nodes than there are)
+    ;
+        Ns is N-1,                      % Reduce N with one, because we index starting at 0
+        numlist(0, Ns, Xs),             % Generate a list containing every number ranging from 0 to N-1
+        uniquePermutation(K, Xs, Ls),   % Generate all unique permutations of size N from the previously generated list
+        allConnected(Ls)                % Check if all nodes in permutation are connected
+    ).
+    
 
 
 /*
@@ -30,11 +36,12 @@ findKClique(K) :-
     modified while loop implementation from: https://stackoverflow.com/questions/22885622/how-can-i-simulate-a-while-loop-in-prolog-with-unchangeable-conditions
 */
 dumbFindLargest(K, KFinal) :-
-    (not(once(findKClique(K))) ->
-         KFinal is K-1
+    (
+        not(once(findKClique(K))) -> 
+        KFinal is K-1                   % Check if there isn't a K-clique present, if so return K-1 as KFinal value
     ;
-         K1 is K+1,
-         dumbFindLargest(K1, KFinal)
+        K1 is K+1,                      % Increment K with one
+        dumbFindLargest(K1, KFinal)     % Loop with increased K value
     ).
 
 
@@ -45,42 +52,62 @@ dumbFindLargest(K, KFinal) :-
     code to generate all possible UNIQUE permutations with repetition
     modified all permutations from: https://stackoverflow.com/questions/43102965/prolog-how-to-create-all-possible-permutations-with-repetition-give-a-list-of-o
 */
-eval([],_).
-eval([H|T],Set):- member(H,Set), eval(T,Set), not(member(H, T)).
-uniquePermutation(N, Set, L):- length(L,N), eval(L,Set).
+uniquePermutation(N, Set, L):- 
+    length(L,N),        % Make sure the returned permutation list is of size N
+    eval(L,Set).        % Construct permutation
+
+eval([],_).             % Start of recursive call
+eval([H|T],Set):- 
+    member(H,Set),      % Choose H as a member of the set of allowed values
+    eval(T,Set),        % Recursively call function for the T(ail), making sure all values in tail are also in the allowed set
+    not(member(H, T)).  % Check that H is not a value already present in T, make sure there are only unique values in the permutation
 
 
 /*
     Check if all nodes are connected
 */
-allConnected([]).
-allConnected([A|L]) :- isConnected(A, L), allConnected(L).
+allConnected([]).       % Start of recursive call
+allConnected([A|L]) :- 
+    isConnected(A, L),  % Check if A is connected to all elements in L (tail)
+    allConnected(L).    % Recursively call check for L
 
-isConnected(_, []).
-isConnected(A, [F|R]) :- isEdge(A, F), isConnected(A, R).
+isConnected(_, []).     % Start of recursive call
+isConnected(A, [F|R]) :- 
+    isEdge(A, F),       % Checks if there is an edge between node A and F
+    isConnected(A, R).  % Recursively call check for R
+
 
 /*
     Gets length of list
 */
-listLength([], 0).
-listLength([_|Xs], L) :- listLength(Xs,N), L is N+1.
+listLength([], 0).      % Start of recursive call
+listLength([_|Xs], L) :- 
+    listLength(Xs,N),   % Iteratively loop over list to get length (keeps going until there is no more head)
+    L is N+1.           % Add one to previous length value
 
 
 /*
     getIndex1D(Y, L, I)
     Put element at index Y for 1D array L in variable I 
 */
-getIndex1D(_, [], _) :- false. 
-getIndex1D(0, [I|_], I).
-getIndex1D(Y, [_|L], I) :- NEW_Y is Y-1, getIndex1D(NEW_Y, L, I).
+getIndex1D(_, [], _) :- false.  % Out of bounds check
+getIndex1D(0, [I|_], I).        % Start of recursive call, return current head (found index)
+getIndex1D(Y, [_|L], I) :- 
+    NEW_Y is Y-1,               % Decrement Y value by one
+    getIndex1D(NEW_Y, L, I).    % Loop over list until Y value is 0
+
 
 /*
     getIndex2D(X, Y, L, I)
     Put element at index X,Y for 2D array L in variable I 
 */
-getIndex2D(_, _, [], _) :- false. 
-getIndex2D(0, Y, [L|_], I) :- getIndex1D(Y, L, I).
-getIndex2D(X, Y, [_|L], I) :- NEW_X is X-1, getIndex2D(NEW_X, Y, L, I).
+getIndex2D(_, _, [], _) :- false.   % Out of bounds check
+getIndex2D(0, Y, [L|_], I) :- 
+    getIndex1D(Y, L, I).            % Found X index, look for Y
+getIndex2D(X, Y, [_|L], I) :- 
+    NEW_X is X-1,                   % Decrement X value by one
+    getIndex2D(NEW_X, Y, L, I).     % Loop over list until X value is 0
+
 
 /*
     Checks if input is 1
@@ -88,10 +115,11 @@ getIndex2D(X, Y, [_|L], I) :- NEW_X is X-1, getIndex2D(NEW_X, Y, L, I).
 isOne(1) :- true.
 isOne(_) :- false.
 
+
 /*
     Determines if there is an edge between node X and Y
 */
 isEdge(X, Y) :- 
-    mygraph(L),
-    getIndex2D(X, Y, L, I),
-    isOne(I).
+    mygraph(L),                 % Loads graph in L
+    getIndex2D(X, Y, L, I),     % Find indexed value
+    isOne(I).                   % Checks if there is an edge between the two
